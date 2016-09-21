93e2f3e8327e9cdd32fa088c466a776ca3d3ddbc
Aimen Khakwani and Katy Henning <aimen@>
2016-09-21 10:43:10 -0700

    Add passing tests for Restaurants

---
+++ src/Restaurant.php
@@ -0,0 +1,70 @@
+<?php
+    class Restaurant
+    {
+        private $id;
+        private $cuisine_id;
+        private $name;
+        private $rate;
+
+        function __construct($id, $cuisine_id, $name, $rate)
+        {
+            $this->id = $id;
+            $this->cuisine_id = $cuisine_id;
+            $this->name = $name;
+            $this->rate = $rate;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getCuisineId()
+        {
+            return $this->cuisine_id;
+        }
+
+        function setName($name)
+        {
+            $this->name = (string) $name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getRate()
+        {
+            return $this->rate;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO restaurants (cuisine_id, name, rate) VALUES ({$this->getCuisineId()}, '{$this->getName()}', {$this->getRate()})");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_restaurants = $GLOBALS['DB']->query("SELECT * FROM restaurants;");
+            $restaurants = array();
+
+            foreach($returned_restaurants as $restaurant){
+                $id = $restaurant['id'];
+                $cuisine_id = $restaurant['cuisine_id'];
+                $name = $restaurant['name'];
+                $rate = $restaurant['rate'];
+                $new_restaurant = New Restaurant($id, $cuisine_id, $name, $rate);
+                array_push($restaurants, $new_restaurant);
+            }
+            return $restaurants;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM restaurants;");
+        }
+    }
+
+?>
---
+++ tests/RestaurantTest.php
@@ -0,0 +1,159 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Restaurant.php";
+
+    $server = 'mysql:host=localhost;dbname=best_restaurants_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class RestaurantTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown(){
+            Restaurant::deleteAll();
+        }
+
+        function test_getId()
+        {
+            //arrange
+            $id = 2;
+            $cuisine_id = 1;
+            $name = "TALKO Taco";
+            $rate = 4;
+            $restaurant = new Restaurant($id, $cuisine_id, $name, $rate);
+
+            //act
+            $result = $restaurant->getId();
+
+            //assert
+            $this->assertEquals($id, $result);
+
+        }
+
+        function test_getCuisineId()
+        {
+            //arrange
+            $id = 2;
+            $cuisine_id = 1;
+            $name = "TALKO Taco";
+            $rate = 4;
+            $restaurant = new Restaurant($id, $cuisine_id, $name, $rate);
+
+            //act
+            $result = $restaurant->getCuisineId();
+
+            //assert
+            $this->assertEquals($cuisine_id, $result);
+
+        }
+
+        function test_getName()
+        {
+            //arrange
+            $id = 2;
+            $cuisine_id = 1;
+            $name = "TALKO Taco";
+            $rate = 4;
+            $restaurant = new Restaurant($id, $cuisine_id, $name, $rate);
+
+            //act
+            $result = $restaurant->getName();
+
+            //assert
+            $this->assertEquals($name, $result);
+
+        }
+
+        function test_getRate()
+        {
+            //arrange
+            $id = 2;
+            $cuisine_id = 1;
+            $name = "TALKO Taco";
+            $rate = 4;
+            $restaurant = new Restaurant($id, $cuisine_id, $name, $rate);
+
+            //act
+            $result = $restaurant->getRate();
+
+            //assert
+            $this->assertEquals($rate, $result);
+
+        }
+
+        function test_save()
+        {
+            //arrange
+            $id = 2;
+            $cuisine_id = 1;
+            $name = "TALKO Taco";
+            $rate = 4;
+            $restaurant = new Restaurant($id, $cuisine_id, $name, $rate);
+            $restaurant->save();
+
+            //act
+            $result = $restaurant->getAll();
+
+            //assert
+            $this->assertEquals($restaurant, $result[0]);
+
+        }
+
+        function test_getAll()
+        {
+            //arrange
+            $id = 2;
+            $cuisine_id = 1;
+            $name = "TALKO Taco";
+            $rate = 4;
+            $restaurant = new Restaurant($id, $cuisine_id, $name, $rate);
+            $restaurant->save();
+
+            $id = 2;
+            $cuisine_id = 1;
+            $name = "Pizzeria";
+            $rate = 4;
+            $restaurant_two = new Restaurant($id, $cuisine_id, $name, $rate);
+            $restaurant_two->save();
+
+            //act
+            $result = Restaurant::getAll();
+
+            //assert
+            $this->assertEquals([$restaurant, $restaurant_two], $result);
+
+        }
+
+        function test_deleteAll()
+        {
+            //arrange
+            $id = 2;
+            $cuisine_id = 1;
+            $name = "TALKO Taco";
+            $rate = 4;
+            $restaurant = new Restaurant($id, $cuisine_id, $name, $rate);
+            $restaurant->save();
+
+            $id = 2;
+            $cuisine_id = 1;
+            $name = "Pizzeria";
+            $rate = 4;
+            $restaurant_two = new Restaurant($id, $cuisine_id, $name, $rate);
+            $restaurant_two->save();
+
+            //act
+            $result = Restaurant::deleteAll();
+
+            //assert
+            $result = Restaurant::getAll();
+            $this->assertEquals([], $result);
+
+        }
+
+    }
+
+ ?>
