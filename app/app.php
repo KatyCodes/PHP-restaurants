9eaf90f2933a434a8873b7cdb9576574ce738150
Aimen Khakwani and Katy Henning <katy@>
2016-09-21 13:30:13 -0700

    Add twig files

--- app/app.php
+++ app/app.php
@@ -1,14 +1,15 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/.php";
+    require_once __DIR__."/../src/Cuisine.php";
+    require_once __DIR__."/../src/Restaurant.php";
 
     use Symfony\Component\Debug\Debug;
     Debug::enable();
     $app = new Silex\Application();
     $app['debug'] = true;
 
-    $server = 'mysql:host=localhost;dbname=';
+    $server = 'mysql:host=localhost;dbname=best_restaurants';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -18,7 +19,13 @@
     ));
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.html.twig', array('' =>  ::getAll()));
+        return $app['twig']->render('index.html.twig', array('cuisines' =>  Cuisine::getAll()));
+    });
+
+    $app->post("/addCuisine", function() use ($app) {
+        $cuisine = new Cuisine($id= null, $_POST['cuisine']);
+        $cuisine->save();
+        return $app['twig']->render('addCuisine.html.twig', array('cuisines' =>  Cuisine::getAll()));
     });
 
     return $app;
